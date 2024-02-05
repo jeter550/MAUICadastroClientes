6f07df8e792903ce6762e0c237f2f64b27d16c91
Jéter Ornelas <jeteroliveira@hotmail.com>
2024-02-04 23:49:20 -0300

    Criação do projeto e confiugrações iniciais

---
+++ src/CadastroDeClientes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CadastroDeClientes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
