d7fb183c47a4b10330dde1b04ce1578af4ae5830
Jéter Ornelas <jeteroliveira@hotmail.com>
2024-02-06 17:33:54 -0300

    projeto view model

---
+++ src/ViewModel/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace ViewModel
+{
+    // All the code in this file is included in all platforms.
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged<T>(Expression<Func<T>> expressao)
+        {
+            string name = ((MemberExpression)expressao.Body).Member.Name;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private Page _pageContainer;
+        public Page PageContainer
+        {
+            get
+            {
+                return _pageContainer;
+            }
+            set
+            {
+                _pageContainer = value;
+                OnPropertyChanged(() => PageContainer);
+            }
+        }
+    }
+}
